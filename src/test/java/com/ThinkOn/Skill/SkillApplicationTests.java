28eb1dab0344719cf8dcc74a5be91eb4d04677ae
Connectakhilb <connect.akhilb@outlook.com>
2024-10-12 13:52:54 -0400

    first commit

---
+++ src/test/java/com/ThinkOn/Skill/SkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThinkOn.Skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
